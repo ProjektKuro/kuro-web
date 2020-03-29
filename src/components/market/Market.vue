ccbdaa225e8abf4dcead82b562c3c656d67d42b4
tlahmann <tobiaslahmann@gmail.com>
2020-03-29 18:23:42 +0200

    Show address postal code and city in market component

--- src/components/market/Market.vue
+++ src/components/market/Market.vue
@@ -1,70 +1,72 @@
 <template>
-    <div class="market">
-        <img class="shop-image" src="@/assets/ic_markt.svg" alt="shop-logo">
-        <div class="shop-text">
-          <span class="shop-name" v:bind="prop-name">{{ name }}</span>
-          <span class="shop-address" v:bind="prop-address">{{ address }}</span>
-        </div>
+  <div class="market">
+    <img class="shop-image" src="@/assets/ic_markt.svg" alt="shop-logo" />
+    <div class="shop-text">
+      <span class="shop-name" v:bind="prop-name">{{ name }}</span>
+      <span
+        class="shop-address"
+        v:bind="prop-address"
+      >{{ (address || {address: 'noAddress'}).address }}</span>
+      <small class="shop-address" v:bind="prop-address">{{ (address || {postalCode: ''}).postalCode }} {{ (address || {city: ''}).city }}</small>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Market",
-    address: "",
-    props: {
-        name: String, 
-        address: String,
-    },
-    // data: () => {
-    //     return {};
-    // },
-    methods: {
-
-    }
-}
+  name: "Market",
+  address: {},
+  props: {
+    name: String,
+    address: {}
+  },
+  // data: () => {
+  //     return {};
+  // },
+  methods: {}
+};
 </script>
 
 <style scoped>
 .market {
-    display: flex;
-    flex-direction: row;
-    border: 1px solid lightgray;
-    border-radius: 5px;
-    box-shadow: 2px 4px 6px 1px rgba(0,0,0,0.2);
-    transition: 0.3s;
-    padding: 0.5rem;
-    margin: 0.5rem auto;
-    background-color: #c6e2e6;
+  display: flex;
+  flex-direction: row;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  box-shadow: 2px 4px 6px 1px rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  padding: 0.5rem;
+  margin: 0.5rem auto;
+  background-color: #c6e2e6;
 }
 
 .market:hover {
-    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-    border: 1px solid lightgray;
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  border: 1px solid lightgray;
 }
 
 .shop-image {
-    margin-left: 0.75rem;
-    max-width: 3.5rem;
+  margin-left: 0.75rem;
+  max-width: 3.5rem;
 }
 
 .shop-text {
-    margin: 0.5rem 0.75rem;
-    display: block;
+  margin: 0.5rem 0.75rem;
+  display: block;
 }
 
 .shop-text .shop-name,
 .shop-text .shop-address {
-    color: #0f576d;
+  color: #0f576d;
 }
 
 .shop-name {
-    display: block;
-    font-weight: bold;
-    text-transform: uppercase;
+  display: block;
+  font-weight: bold;
+  text-transform: uppercase;
 }
 
 .shop-address {
-    display: block;
+  display: block;
 }
 </style>
