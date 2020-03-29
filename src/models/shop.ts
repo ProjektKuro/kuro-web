bd603d44687febed940ba3f335ce7d714b91c926
Sven Patrick Meier <sven.meier@uni-ulm.de>
2020-03-30 00:15:00 +0200

    add Position to Shop

--- src/models/shop.ts
+++ src/models/shop.ts
@@ -1,15 +1,21 @@
 import { Resource } from './resource';
 import { Address } from './address';
 import { Product } from './product';
+interface IPosition {
+    latitude: number;
+    longitude: number;
+}
 type Products = Product[];
 export class Shop extends Resource {
     private _name: string;
     private _address: Address;
+    private _position: IPosition;
     private _products: Products;
     constructor (json: any) {
        super(json);
        this._name = json?.name;
        this._address = json?.address;
+       this._position = json?.position;
        this._products = json?.products;
     }
     get address(): Address {
@@ -17,6 +23,9 @@ export class Shop extends Resource {
     }
     set address(address: Address) {
         this._address = address;
+    }
+    get position(): IPosition {
+        return this._position;
     }
     get products(): Products {
         return this._products;
