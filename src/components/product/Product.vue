7f22eb73a0a276b51de1e29ab638447aec63f49f
tlahmann <tobiaslahmann@gmail.com>
2020-03-29 21:10:07 +0200

    Fix products grid display

--- src/components/product/Product.vue
+++ src/components/product/Product.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="product">
-    <img class="product-image" src="@/assets/logo.svg" alt="product-logo" />
-    <span class="product-name" v:bind="prop-name">{{ name }}</span>
+  <div class="product-container">
+    <div class="product">
+      <img class="product-image" src="@/assets/logo.svg" alt="product-logo" />
+      <span class="product-name" v:bind="prop-name">{{ name }}</span>
+    </div>
   </div>
 </template>
 
@@ -23,31 +25,34 @@ export default {
   margin: 0.1rem auto;
   max-width: 4rem;
 }
+.product-container {
+  width: calc(100% / 4);
+}
 .product {
   display: flex;
   flex-direction: column;
-  min-width: 30%;
-  max-width: 30%;
-  border: 1px solid transparent;
+  width: 94%;
+  height: calc(90% - 2px);
+  border: 1px solid #c6e2e6;
   border-radius: 5px;
+  -webkit-box-shadow: 2px 4px 6px 1px rgba(0, 0, 0, 0.2);
   box-shadow: 2px 4px 6px 1px rgba(0, 0, 0, 0.2);
+  -webkit-transition: 0.3s;
   transition: 0.3s;
-  padding: 0.5rem;
-  margin: 0.5rem 0.1rem;
+  margin: calc(3% - 2px);
   background-color: #c6e2e6;
 }
-
 .product:hover {
   box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
   border: 1px solid lightgray;
 }
-
 .product p,
 span {
   color: #000;
 }
-.product {
-  text-align: center;
-  margin: 0.5rem 0.25rem;
+@media only screen and (max-width: 768px) {
+  .product-container {
+    width: calc(100% / 2);
+  }
 }
 </style>
