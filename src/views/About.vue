7be44fe80524011c6e0082a31cd5245058748dac
tlahmann <tobiaslahmann@gmail.com>
2020-03-30 18:09:23 +0200

    Add logo to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,18 @@
   <div class="about">
     <span class="heading">{{$t('Info.AboutKuro')}}</span>
     <p>{{$t('Info.AboutKuroDescription')}}</p>
+    <p>
+      {{$t('Info.Created.First')}}
+      <a
+        href="https://wirvsvirushackathon.org/"
+        target="_blank"
+      >{{$t('Info.Created.Link')}}</a>
+    </p>
+    <img
+      class="wir-vs-virus-logo"
+      src="../assets/Logo_WirVsVirus.png"
+      :alt="$t('Info.WirVsVirusLogo')"
+    />
   </div>
 </template>
 
@@ -13,5 +25,12 @@
 .heading {
   font-weight: bold;
   font-size: 1.5rem;
+}
+
+.wir-vs-virus-logo {
+  background-color: #20beef;
+  width: 100%;
+  max-width: 720px;
+  margin: 0 auto;
 }
 </style>
