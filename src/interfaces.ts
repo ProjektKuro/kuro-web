a1b0d7ed0614e28c74b059ba1815990cb94af501
Sven Patrick Meier <sven.meier@uni-ulm.de>
2020-04-05 19:48:43 +0200

    add application wide interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,7 +1,7 @@
-export type TProduct = IProduct[];
+export type TProducts = IProduct[];
 
 export interface IProductState {
-    products: TProduct;
+    products: TProducts;
 }
 
 export interface IProduct {
@@ -9,4 +9,17 @@ export interface IProduct {
     description: string;
     name: string;
     productId: string;
+}
+
+export interface IApiProduct {
+    categories: string[];
+    description: string;
+    name: string;
+    _id: string;
+    updatedAt: string;
+    createdAt: string;
+    _cls: string;
+}
+export interface IApiResponse {
+    products: IApiProduct[]
 }
