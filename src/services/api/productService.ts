a6f41a61e2544d73a0b78acf991d2c6626434cc0
Sven Patrick Meier <sven.meier@uni-ulm.de>
2020-04-05 19:47:41 +0200

    create a service for products. Resolves #15

--- src/services/api/productService.ts
+++ src/services/api/productService.ts
@@ -1,8 +1,19 @@
 import { BaseApiService } from "./base";
+import { AxiosResponse } from 'axios';
 export class ProductService {
 
-    static getAllProducts = async (): Promise<void> => {
-        return await BaseApiService.instance.get("/products");
+    static getAllProducts = async (): Promise<AxiosResponse> => {
+        return await BaseApiService.instance.read("/products");
     };
+
+    static getProductById = async (productId: string) => {
+        return await BaseApiService.instance.read(`/products/${productId}`)
+    };
+
+    static updateProduct = async (product: any) => {
+        const { productId } = product;
+        return await BaseApiService.instance.update(`/products/${productId}`, product);
+    }
+
 
 }
