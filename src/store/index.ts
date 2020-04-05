e972113feebdfdf7a256b36a6b3821e206280e61
Sven Patrick Meier <sven.meier@uni-ulm.de>
2020-04-05 19:48:19 +0200

    create product store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import { createVuexStore, Module, Getter } from "vuex-simple";
+import { ProductService } from '@/services/api/productService';
+import { ProductStore } from './productStore';
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+class Store {
+  
+  @Module()
+  public products = new ProductStore();
+
+  @Getter()
+  public get allProducts() {
+    return this.products.allProducts;
   }
-})
+  
+}
+
+export default createVuexStore(new Store(), {
+  strict: false,
+  modules: {},
+  plugins: []
+});
--- src/store/productStore.ts
+++ src/store/productStore.ts
@@ -1,9 +1,44 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import { IProductState } from '../interfaces';
+import {
+  TProducts,
+  IApiResponse
+} from "../interfaces";
+import { ProductService } from "@/services/api/productService";
+import { State, Action, Mutation, Getter, Module } from "vuex-simple";
+import { AxiosResponse } from "axios";
+export class ProductStore {
+  @State()
+  public _products: TProducts;
+  @State()
+  public productsLoadingErr: string;
 
-Vue.use(Vuex);
+  constructor() {
+    this._products = [];
+    this.productsLoadingErr = "";
+  }
 
-const state: IProductState = {
-    products: []
+  @Getter()
+  get allProducts(): TProducts {
+    return this._products;
+  }
+
+  @Mutation()
+  async fetchAllProducts() {
+    const productsResponse: AxiosResponse<IApiResponse> = await ProductService.getAllProducts();
+    const { products: apiProducts } = productsResponse.data;
+    let products: TProducts = [];
+    for (const product of apiProducts) {
+      products.push({
+        categories: product.categories,
+        name: product.name,
+        description: product.description,
+        productId: product._id
+      });
+    }
+    this.setProducts(products);
+  }
+
+  @Action()
+  setProducts(products: TProducts) {
+    this._products = products;
+  }
 }
